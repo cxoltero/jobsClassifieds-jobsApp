324016e6b341cdbe1c3d72c6f9083920520100e3
cxoltero <carlosh95@gmail.com>
2014-10-24 12:21:16 -0400

    added the association for the models, a category has many jobs, and a job belongs to a category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024161615) do
+ActiveRecord::Schema.define(version: 20141024161835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141024161615) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
